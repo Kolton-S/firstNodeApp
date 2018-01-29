52b3a87581c8553c062eaecfabf3406bd4396409
Simpson <k_simpson22@tss-hr420-dm06.fc.ca>
2018-01-29 12:34:08 -0500

    Added socket

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express'); // just like an include or require with PHP
 const app = express(); // create an instance of our application via simpleExpress
+const io = require('socket.io')(); // get socket.io
 
 app.use(express.static('public'));
 
@@ -8,6 +9,11 @@ app.use(require('./routes/index'));
 app.use(require('./routes/contact'));
 app.use(require('./routes/users'));
 
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
   console.log('listening on port 3000!');
+});
+
+io.attach(server);
+io.on('connection', () =>{
+  console.log('connected user');
 });
